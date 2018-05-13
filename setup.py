c265d14a3dc29537b85e71ae881d49e7dbad1b67
Stefan Kopieczek <stefankopieczek@gmail.com>
2018-05-13 21:16:47 +0100

    Add skeleton project layout
    - License
    - Module (cloudcanary)
    - setup.py
    - requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cloud Canary',
+      version='0.1',
+      description='VM uptime alarm daemon',
+      author='Stefan Kopieczek',
+      author_email='stefankopieczek@gmail.com',
+      packages=['cloudcanary']
+     )
